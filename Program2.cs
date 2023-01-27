64df3704877658a18e4d87c350327c42d34f3ddc
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 20:01:29 +0330

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        private int R_max;
+        private int R_min;
+
+        public int r_max
+        {
+            get { return R_max; }
+            set { R_max = value; }
+        }
+        public int r_min
+        {
+            get { return R_min; }
+            set { R_min = value; }
+        }
+        public void cal() { }
+        public void cal(int a, int b)
+        {
+            R_max = a;
+            R_min = b;
+        }
+        public double masahat()
+        {
+            double x = R_max * R_min * 3.14;
+            return x;
+        }
+    }
+        class cls
+        {
+            public static void main()
+            {
+                Program test = new Program();
+                double result = test.masahat();
+                Console.WriteLine(result);
+            }
+        }
+}
